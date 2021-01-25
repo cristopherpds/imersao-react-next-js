ba17a2a77fe966dd9ab7fe666f248b049b1ee995
Cristopher Paiva <josuepaiva7@gmail.com>
2021-01-25 13:45:02 -0300

    fix bugs

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components'
+import Head from 'next/head'
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '..//src/components/GitHubCorner';
 import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
 
 const Title = styled.h1`
   font-size: 50px;
@@ -32,24 +34,31 @@ margin: auto 10%;
 export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>{db.title} </title>
+        <meta property="og:image" content={db.bg} key="title" />
+      </Head>
     <QuizContainer>
+      <QuizLogo backgroundImage={db.bg} />
       <Widget>
         <Widget.Header>
-          <h1>League Of Legends</h1>
+          <h1>{db.title} </h1>
         </Widget.Header>
         <Widget.Content>
-        <p>Ahhhh vamos ser amigos para sempre...</p>
-      </Widget.Content>
+          <p>{db.description} </p>
+          <Widget.Input placeholder="..."/>
+        </Widget.Content>
       </Widget>
+
       <Widget>
         <Widget.Content>
-        <h1>Quiz da galera</h1>
-        <p>Ahhhh vamos ser amigos para sempre...</p>
+          <h1>Quiz da galera</h1>
+          <p>Ahhhh vamos ser amigos para sempre...</p>
         </Widget.Content>
       </Widget>
       <Footer/>
     </QuizContainer>
-    <GitHubCorner projectUrl="https://github.com/cristopherpds"/>
+    <GitHubCorner projectUrl="https://github.com/cristopherpds/lol-quiz"/>
     </QuizBackground>
   );
 }
--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -21,6 +21,18 @@ const Widget = styled.div`
   }
 `;
 
+
+Widget.Input = styled.input`
+  width: 100%;
+  font-size: 15px;
+  color: white;
+  padding: 10px;
+  outline: 0;
+  border: 1px solid ${({ theme }) => theme.colors.primary};
+  border-radius: 5px;
+  background-color: transparent;
+`
+
 Widget.Content = styled.div`
   padding: 24px 32px 32px 32px;
   & > *:first-child {
